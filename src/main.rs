0e4bd554cfe86631abfd181cd17d7817b009ba27
Max Smirnov <masynchin@gmail.com>
2022-08-19 03:32:12 +0300

    Fix indentation

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod app;
 use app::cmd;
 
 fn main() {
-   match cmd().get_matches().subcommand() {
-      Some(("new", _)) => println!("Creating CHANGELOG..."),
-      _ => unreachable!("clap should ensure we don't get here"),
-   }
+    match cmd().get_matches().subcommand() {
+        Some(("new", _)) => println!("Creating CHANGELOG..."),
+        _ => unreachable!("clap should ensure we don't get here"),
+    }
 }
