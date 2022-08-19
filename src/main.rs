b8ee32b11694aca9e1fe5d662b2455d81983b046
Max Smirnov <masynchin@gmail.com>
2022-08-19 06:28:10 +0300

    Make new command work

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+mod app;
 mod cmd;
 
+use std::{fs::OpenOptions};
+
+use app::new;
 use cmd::cmd;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     match cmd().get_matches().subcommand() {
-        Some(("new", _)) => println!("Creating CHANGELOG..."),
+        Some(("new", _)) => new(OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("CHANGELOG.md")?
+        ),
         _ => unreachable!("clap should ensure we don't get here"),
     }
 }
