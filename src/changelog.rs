d2dce2ec398215174f5978ed59b3f6fc64539eab
Max Smirnov <masynchin@gmail.com>
2022-08-27 23:23:38 +0300

    Use format!

--- src/changelog.rs
+++ src/changelog.rs
@@ -12,19 +12,19 @@ impl Changelog {
     }
 
     pub fn as_string(&self) -> String {
-        let mut title: String = "# Changelog
+        let title: String = "# Changelog
 
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
-and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).\n"
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html)."
             .into();
 
-        title.push_str("\n");
-        title.push_str(self.versions.as_string().as_str());
-        title.push_str("\n");
-
-        title
+        format!(
+            "{}\n\n{}\n",
+            title,
+            self.versions.as_string()
+        )
     }
 }
 
